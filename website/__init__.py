9c181ef7c664d6b09a509833eeeef4b691c3f43d
user4302 <4ngelo@outlook.com>
2021-10-14 21:41:47 +0530

    created flask app

--- website/__init__.py
+++ website/__init__.py
@@ -1,2 +1,9 @@
 # makes the website folder into a python package
 # when importing the website folder, the commands in this file will be run
+
+from flask import Flask
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = '^R795wEm#Y!$jzS8#*9%3pu*ob6huhmA$^f4N7mNK2%J9K2C7ZWZb@9@WU5Nd553'
+    return app
