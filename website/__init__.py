5b9c60aa48bdad7cdfdbd9a76bfe297c639b6165
user4302 <4ngelo@outlook.com>
2021-10-14 22:12:47 +0530

    created routes and views

--- website/__init__.py
+++ website/__init__.py
@@ -6,4 +6,11 @@ from flask import Flask
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = '^R795wEm#Y!$jzS8#*9%3pu*ob6huhmA$^f4N7mNK2%J9K2C7ZWZb@9@WU5Nd553'
+    
+    from .views import views
+    from .auth import auth
+
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
+
     return app
