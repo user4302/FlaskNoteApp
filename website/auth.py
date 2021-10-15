5da6081753db7cf7ad87f26b95f43cf733128cd2
user4302 <4ngelo@outlook.com>
2021-10-15 12:00:40 +0530

    HTTP Requests

--- website/auth.py
+++ website/auth.py
@@ -1,8 +1,8 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
     return render_template("login.html", boolean=True)
 
@@ -10,6 +10,6 @@ def login():
 def logout():
     return render_template("logout.html")
     
-@auth.route('/register')
+@auth.route('/register', methods=['GET', 'POST'])
 def register():
     return render_template("register.html")
