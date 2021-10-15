69a87955aeef713b78b85389215053e340c54d5b
user4302 <4ngelo@outlook.com>
2021-10-15 12:05:03 +0530

    Handling POST Requests

--- website/auth.py
+++ website/auth.py
@@ -12,4 +12,22 @@ def logout():
     
 @auth.route('/register', methods=['GET', 'POST'])
 def register():
+    
+    if request.method == 'POST':
+        email = request.form.get('email')
+        firstName = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if len(email) < 4:
+            pass
+        elif len(firstName) <2:
+            pass
+        elif password1 != password2:
+            pass
+        elif len(password1) < 7:
+            pass
+        else:
+            pass
+
     return render_template("register.html")
