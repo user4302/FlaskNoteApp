2cf19f3085319520f8304a6d29523b5f855ec85d
user4302 <4ngelo@outlook.com>
2021-10-15 12:07:47 +0530

    Flask SqlAlchemy Setup

--- website/__init__.py
+++ website/__init__.py
@@ -2,10 +2,16 @@
 # when importing the website folder, the commands in this file will be run
 
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = '^R795wEm#Y!$jzS8#*9%3pu*ob6huhmA$^f4N7mNK2%J9K2C7ZWZb@9@WU5Nd553'
+    app.config['SQLALCHEMY_DATABASE_URI'] - f'sqlite:///{DB_NAME}'
+    db.init_app(app)
     
     from .views import views
     from .auth import auth
