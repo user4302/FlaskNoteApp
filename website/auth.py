71edda08031d4901dd22f4b1a56332d85fe32e96
user4302 <4ngelo@outlook.com>
2021-10-15 10:59:16 +0530

    Jinja Templating and HTML templates

--- website/auth.py
+++ website/auth.py
@@ -1,15 +1,15 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 auth = Blueprint('auth', __name__)
 
 @auth.route('/login')
 def login():
-    return "<p>login</p>"
+    return render_template("login.html", boolean=True)
 
 @auth.route('/logout')
 def logout():
-    return "<p>logout</p>"
+    return render_template("logout.html")
     
 @auth.route('/register')
 def register():
-    return "<p>register</p>"
+    return render_template("register.html")
--- website/views.py
+++ website/views.py
@@ -1,10 +1,10 @@
 # the views/url endpoints of the frontend of the website
 # we define in this file that this file is a "blueprint" of this application
 
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 views = Blueprint('views', __name__)
 
 @views.route('/')
 def home():
-    return "<h1>FlaskNoteApp Home Page</h1>"
+    return render_template("home.html")
